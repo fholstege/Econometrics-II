6384d7070702c1764c3d49bcbcc3ccb8c749c865
fholstege <31654652+fholstege@users.noreply.github.com>
2021-01-20 12:19:59 +0100

    MDE

--- Assignment 3/Assignment3_code.R
+++ Assignment 3/Assignment3_code.R
@@ -4,7 +4,9 @@ pacman::p_load(plm,
                Formula,
                car,
                clubSandwich,
-               lmtest)
+               lmtest,
+               foreign, 
+               tidyverse)
 
 
 # create df with treatment effects
@@ -13,4 +15,100 @@ row.names(dfTreatment) <- c("Purple", "Blue", "Green")
 
 # average treatment effect per group
 ATE_group <- dfTreatment$Avg_Outcome_treated - dfTreatment$Avg_Outcome_control
+
+ATE_treated <- sum(dfTreatment$N_treated/sum(dfTreatment$N_treated) * dfTreatment$Avg_Outcome_treated)
+ATE_control <- sum(dfTreatment$N_control/sum(dfTreatment$N_control) * dfTreatment$Avg_Outcome_control)
+ATE_treated - ATE_control
+
+
+
+dfBonus <- read.dta("Data/bonus.dta")
+
+dfBonus$category <- ifelse(dfBonus$bonus500 == 1, "Low-reward", ifelse(dfBonus$bonus1500 == 1, "High-reward", "Control"))
+
+dfBonus %>% 
+  group_by(category) %>% 
+  summarise(perc_passed_year1 = sum(pass)/ n(), 
+            avg_myeduc = mean(myeduc),
+            avg_fyeduc = mean(fyeduc), 
+            avg_p0 = mean(p0),
+            math = mean(math[!is.na(math)]),
+            perc_job = sum(job[!is.na(job)])/n(),
+            avg_effort = mean(effort[!is.na(effort)]),
+            total = n())
+
+
+
+LPM_simple <- lm(pass ~ category, data=dfBonus)
+LPM_addedRegressors <- lm(pass ~ category + math + fyeduc + p0, data=dfBonus)
+LPM_allRegressors <- lm(pass ~ category + math + fyeduc + p0 + effort + job, data=dfBonus)
+
+
+mX <- dfBonus %>% select(math, fyeduc, p0, effort, job) %>% as.matrix() %>% na.omit()
+cor(mX)
+vif(LPM_allRegressors)
+
+summary(LPM_simple)
+summary(LPM_addedRegressors)
+summary(LPM_allRegressors)
+
+LPM_drop <- lm(dropout ~ category + math + fyeduc + p0 + effort + job, data=dfBonus)
+LM_pointsYear1 <- lm(stp2001 ~ category + math + fyeduc + p0 + effort + job, data=dfBonus)
+LM_pointsYear3 <- lm(stp2004 ~ category + math + fyeduc + p0 + effort + job, data=dfBonus)
+
+summary(LPM_drop)
+summary(LM_pointsYear1)
+summary(LM_pointsYear3)
+
+length(coef(LPM_allRegressors))
+
+
+
+n <- nrow(dfBonus)
+p <- sum(dfBonus$bonus500) + sum(dfBonus$bonus1500)
+power <- 0.8
+alpha <- 0.05
+
+t_alpha <- qt(1-alpha/2, df=n-8)
+t_q <- qt(1-power, df=n-8)
+t_alpha - t_q
+
+nrow(LPM_allRegressors$model)
+
+df <- LPM_allRegressors$model
+df[,"category"]
+
+get_MDE <- function(Model_exp, sGroup, fAlpha,fPower){
+  
+  # get data used in the model
+  dfExp <- Model_exp$model
+  
+  # get the n for the model
+  n <- nrow(dfExp)
+  
+  # count how many in treatment
+  nTreatment <- sum(dfExp[, sGroup])
+  
+  # get proportion of control
+  p <- (n-nTreatment)/n
+  
+  # get degrees of freedom
+  iDF <- length(Model_exp$coefficients)
+  
+  t_alpha <- qt(1-fAlpha/2, df = iDF)
+  t_q <- qt(1-fPower, df = iDF)
+  
+  
+  
+}
+  
+  
+  
+  
+  
+  
+}
+
+
+
 
